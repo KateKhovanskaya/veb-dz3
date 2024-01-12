90970e40b681321375746261bf2cc57f63be4018
Kate Khovanskaya <KhovanskayaK@yandex.ru>
2024-01-12 13:20:48 +0300

    homework3 done

---
+++ index.js
@@ -0,0 +1,13 @@
+let favouriteFilm = "Superman"
+if (prompt("Ваш любимый фильм:") == favouriteFilm) {
+    alert("Все верно!")
+} else {
+    alert("Попробуйте ещё раз")
+}
+let arr = ["a", "b", "c"]
+document.write(arr)
+arr[0] = "Привет"
+arr[1] = " я твой персональный помощник"
+arr[2] = "я"
+document.write("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;")
+document.write(arr)
